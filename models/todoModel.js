a47eb4827a30adfd006189183ad985641534cd6c
nafiu adegbite <youngguru23@gmail.com>
2022-07-06 06:48:12 +0100

    fix: timestamps

--- models/todoModel.js
+++ models/todoModel.js
@@ -2,10 +2,17 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const TodoSchema = new Schema({
-  title: { type: String, required: true },
-  description: { type: String, required: true },
-  timestamp: { type: Date, default: Date.now },
-});
+const TodoSchema = new Schema(
+  {
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+  },
+  {
+    timestamps: {
+      createdAt: "created_at",
+      updatedAt: "updated_at",
+    },
+  }
+);
 
 module.exports = new mongoose.model("Todo", TodoSchema);
